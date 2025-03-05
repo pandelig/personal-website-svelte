110bb3aa59b085a209d24ab52e611f4396838adb
pandelig <pandelig@gmail.com>
2025-03-05 20:14:52 +0200

    Delete all project related files
    in order to start from the beginning, following the tutorial step by step

--- src/routes/blog/+page.server.js
+++
@@ -1,5 +0,0 @@
-import { loadMetadataOfPosts } from '$lib/functions/loadMetadataOfPosts';
-
-export function load() {
-    return loadMetadataOfPosts('articles');
-}
--- src/routes/blog/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import PostList from '$lib/components/PostList.svelte';
-
-	let { data } = $props();
-</script>
-
-<PostList {...data} />
